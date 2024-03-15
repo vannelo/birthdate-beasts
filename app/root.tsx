9db5927fbe1daab399d635eda00ad71269583c2b
Vannelo <work@vannelo.com>
2024-03-15 14:57:18 -0600

    Add required fields

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,26 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Discover your animal alter-ego" />
+        <meta
+          property="og:image"
+          content="https://birthdate-beasts-ai.vercel.app/img/meta.jpg"
+        />
+        <meta property="og:title" content="Birthdate Beasts AI" />
+        <meta
+          property="og:description"
+          content="Discover your animal alter-ego"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:image"
+          content="https://birthdate-beasts-ai.vercel.app/img/meta.jpg"
+        />
+        <meta property="twitter:title" content="Birthdate Beasts AI" />
+        <meta
+          property="twitter:description"
+          content="Discover your animal alter-ego"
+        />
         <Meta />
         <Links />
       </head>
